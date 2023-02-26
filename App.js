bce4e8875878222b13df35dd022593d3996fdf00
Brian Ye <brian.ye2021@gmail.com>
2023-02-26 18:16:47 -0500

    added calendar packages

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import { NavigationContainer } from '@react-navigation/native';
-import DashboardScreen from "./app/screens/DashboardScreen";
 
 export default function App() {
   return (
