e7f96d5b960b89105858b1c31508e1a4f1a1dafd
Brian Ye <brian.ye2021@gmail.com>
2023-03-04 18:34:25 -0500

    trying to update to firebase v9 *NOT WORKING*

--- app/navigation/firebase.js
+++ app/navigation/firebase.js
@@ -1,11 +1,13 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from "firebase";
+import { initializeApp, getApps, getApp } from "firebase/app"
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+import { getDatabase } from 'firebase/database'; 
+import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseConfig = initializeApp({
   apiKey: "AIzaSyAykkCbsVHJi2aFuGrUFYepEk0gR_mrZCQ",
   authDomain: "member-app-8683b.firebaseapp.com",
   projectId: "member-app-8683b",
@@ -13,16 +15,12 @@ const firebaseConfig = {
   messagingSenderId: "296333616431",
   appId: "1:296333616431:web:bafb79c1ac5238bc80edda",
   measurementId: "G-J10V2VZ3DD"
-};
+});
 
 // Initialize Firebase
-let app;
-if (firebase.apps.length === 0) {
-    app = firebase.initializeApp(firebaseConfig);
-} else {
-    app = firebase.app();
-}
-const auth = firebase.auth();
-// const analytics = getAnalytics(app);
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp(); // To make sure we don't initalize twice https://stackoverflow.com/a/73176837
+
+const auth = getAuth(app);
 
 export { auth };
