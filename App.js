cc0de7f5aedd57bbc96b1a4fabb1ad33915c80d1
Oluwasemilore Adegoroye <oluwasemilore508@gmail.com>
2023-07-10 17:20:58 -0500

    Added the tech support screen

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AuthNavigator from './app/navigation/AuthNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import TechSupportScreen from './app/screens/TechSupportScreen';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AuthNavigator />
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <AuthNavigator />
+    // </NavigationContainer>
+    <TechSupportScreen />
   );
 }
