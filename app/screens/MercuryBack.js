1548f4c4141f30c9d0d4b58fe00b424baad9107d
angelica <150307673+angelicatang07@users.noreply.github.com>
2024-06-10 20:03:36 +0000

    Back Arrow and Total Points Display

--- app/screens/MercuryBack.js
+++ app/screens/MercuryBack.js
@@ -1,15 +1,28 @@
 import React, { useEffect } from 'react';
-import { View, StyleSheet, Text, TouchableWithoutFeedback } from 'react-native';
+import { View, StyleSheet, Text, TouchableWithoutFeedback,TouchableOpacity } from 'react-native';
 import PlanetBack from '../components/PlanetBack';
 import Mercury from '../assets/Mercury.png';
 import Screen from '../components/Screen';
+import { NavigationContainer } from '@react-navigation/native';
 
+export function App() {
+    return (
+      <NavigationContainer>
+        <MercuryBack />
+      </NavigationContainer>
+    );
+  }
 
 function MercuryBack({ navigation }){
     return (
         <Screen>
             <View style={styles.container}>
-                <Text>Hi</Text>
+                <View style={styles.pointsContainer}>
+                        <Text style={styles.pointsText}>{'1800'}</Text>
+                    </View>
+                    <TouchableOpacity onPress={() => {navigation.goBack()} } style={styles.backArrowContainer}>
+                        <Text style={styles.backArrow}>{'<'}</Text>
+                    </TouchableOpacity>
                 <PlanetBack 
                     navigation={navigation}
                     planetPicture={Mercury}
@@ -28,6 +41,43 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+
+    pointsContainer: {
+        position:'absolute',
+        backgroundColor:'#b0bec5',
+        height:25,
+        width:102,
+        right:10,
+        top:16,
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    pointsText: {
+        color: '#ffffff', 
+        fontSize: 20,
+        fontFamily: 'Roboto', 
+        fontWeight: 'bold', 
+        textAlign: 'center',
+    },
+    backArrowContainer: {
+        position:'absolute',
+        width:75,
+        height:35,
+        left:13,
+        padding:0,
+        top:17,
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        zIndex: 1
+    },
+    backArrow: {
+        color:'#ffffff',
+        fontSize:30,
+        fontFamily: 'Roboto', 
+        fontWeight:'700',
+        textAlign: 'center',
+    }, 
 });
 
 export default MercuryBack;
